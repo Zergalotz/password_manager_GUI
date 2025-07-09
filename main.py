5db1016b56a5ac85660afadcbd26552d1fcf1ee8
Stephen Ellis <40131246+Zergalotz@users.noreply.github.com>
2025-07-08 20:34:35 -0500

    base project that other branches will pull. Added readme, main.py file, two new directories (Constants, Shared_Widgets), and new image.

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from Constants import constants
+
+""" -------------------------  Password Generator  ------------------------- """
+
+""" -------------------------    Save Password    -------------------------- """
+
+""" -------------------------       UI Setup      -------------------------- """
+window = Tk()
+window.title(constants.WINDOW_TITLE)
+window.config(width=constants.WINDOW_WIDTH, height=constants.WINDOW_HEIGHT, padx=constants.PAD_X, pady=constants.PAD_Y)
+window.mainloop()
