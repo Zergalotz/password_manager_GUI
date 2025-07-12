f6523c00bd9188b996651fae9e842a1cb7e95f4e
Stephen Ellis <40131246+Zergalotz@users.noreply.github.com>
2025-07-12 00:52:26 -0500

    GUI changes - Added a unit test file and using pytest - Added popup messages for successful adding of pw - Added popup message when fields don't have inputs

--- Shared_Widgets/button_widget.py
+++ Shared_Widgets/button_widget.py
@@ -1,10 +1,8 @@
 import tkinter as tk
-from Constants import constants
 
 class CustomButton(tk.Button):
-    def __init__(self, master, text, command=None, **kwargs):
-        super().__init__(master, text=text, command=command, **kwargs)
-        self.config(padx=10, pady=5, font=(constants.FONT_TYPE, constants.FONT_SIZE, constants.FONT_STYLE), bg=constants.BACKGROUND_COLOR, fg=constants.FOREGROUND_COLOR)
+    def __init__(self, master, text, command=None, font=None, bg=None, fg=None, padx=None, pady=None, **kwargs):
+        super().__init__(master, text=text, command=command, font=font, bg=bg, fg=fg, padx=padx, pady=pady, **kwargs)
 
     def set_text(self, text):
         self.config(text=text)
--- main.py
+++ main.py
@@ -1,49 +1,68 @@
 from tkinter import *
+from tkinter import messagebox
+
 from Constants import constants
 from Shared_Widgets import label_widget, button_widget, input_field
-
 """ -------------------------  Password Generator  ------------------------- """
 def generate_password():
-    # Placeholder for password generation logic
-    print("Password generated!")  # Replace with actual password generation logic
+
+    print("Password generated!")
 """ -------------------------    Save Password    -------------------------- """
 def add_saved_password():
-    # Placeholder for saving password logic
-    print("Password saved!")  # Replace with actual save logic
+    # popup message to confirm saving the password
+    with open("Saved_Passwords/passwords.txt", "a") as file:
+        website = website_input.get()
+        email_username = email_username_input.get()
+        password = password_input.get()
+
+        if len(email_username) == 0 or len(password) == 0:
+            messagebox.showinfo(title="Invalid", message="Please fill in all fields.")
+        else:
+            is_correct = messagebox.askokcancel(title=website, message=f"Do you want to save the password for {website}?\n"
+                                                          f"Email/Username: {email_username}\n"
+                                                          f"Password: {password}\n"
+                                                          f"Is it correct?")
+
+            if is_correct:
+                messagebox.showinfo(title="Info", message="Password saved successfully!")
+                if website and email_username and password:
+                    file.write(f"{website} | {email_username} | {password}\n")
+                    website_input.delete(0, END)
+                    email_username_input.delete(0, END)
+                    password_input.delete(0, END)
+                    website_input.focus()
 """ -------------------------       UI Setup      -------------------------- """
+# Initialize the main window
 window = Tk()
 window.title(constants.WINDOW_TITLE)
 window.config(padx=constants.WINDOW_PAD_X, pady=constants.WINDOW_PAD_Y)
 
+# Widgets, including labels, buttons, and input fields
 canvas = Canvas(width=constants.CANVAS_WIDTH, height=constants.CANVAS_HEIGHT)
-logo_image = PhotoImage(file="./Images/logo.png")  # Ensure the image file is in the same directory
+logo_image = PhotoImage(file="logo.png")  # Ensure the image file is in the same directory
 canvas.create_image(constants.CANVAS_IMAGE_POSITION,image=logo_image)
-generate_button = button_widget.CustomButton(window, text="Generate Password", command=generate_password) #
-add_button = button_widget.CustomButton(window, text="Add", command=add_saved_password) #
+generate_button = button_widget.CustomButton(window, text="Generate", command=generate_password) #
+add_button = button_widget.CustomButton(window, text="Add", command=add_saved_password, width= 27) #
 website_label = label_widget.Label(window, text="Website:")
 email_username_label = label_widget.Label(window, text="Email/Username:")
 password_label = label_widget.Label(window, text="Password:")
 website_input = input_field.CustomInputField(window, placeholder="Website URL", width=constants.INPUT_WIDGET_WIDTH)
 email_username_input = input_field.CustomInputField(window, placeholder="Email or Username", width=constants.INPUT_WIDGET_WIDTH)
-password_input = input_field.CustomInputField(window, placeholder="Password", width=constants.INPUT_WIDGET_WIDTH)
+password_input = input_field.CustomInputField(window, placeholder="Password", width=constants.INPUT_PASSWORD_WIDTH)
 
-canvas.grid(row=0, column=2, columnspan=3)
-generate_button.grid(row=4, column=2, columnspan=3)
-add_button.grid(row=5, column=2, columnspan=3)
-website_label.grid(row=2, column=1)
-website_input.grid(row=2, column=2, columnspan=3)
-email_username_label.grid(row=3, column=1)
-email_username_input.grid(row=3, column=2, columnspan=3)
-password_label.grid(row=4, column=1)
-password_input.grid(row=4, column=2, columnspan=3)
+# Layout the widgets in the window
+canvas.grid(row=0, column=1, columnspan=3)
+generate_button.grid(row=4, column=3,)
+add_button.grid(row=5, column=1, columnspan=3)
+website_label.grid(row=2, column=0)
+website_input.grid(row=2, column=1, columnspan=3)
+website_input.focus()
+email_username_label.grid(row=3, column=0)
+email_username_input.grid(row=3, column=1, columnspan=3)
+email_username_input.focus()
+password_label.grid(row=4, column=0)
+password_input.grid(row=4, column=1)
+password_input.focus()
 
-
-
-
-# canvas.grid()
-# add_button.grid()
-# generate_button.grid()
-
-
-# must be at the end of the file
+# Run the main loop to display the window
 window.mainloop()
